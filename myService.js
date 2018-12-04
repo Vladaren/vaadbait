4b5be72714995a042dbcbb4635e77db7ccbd224c
Vladaren <44053434+Vladaren@users.noreply.github.com>
2018-12-04 18:13:12 +0200

    Delete myService.js

--- myService.js
+++
@@ -1,9 +0,0 @@
-app.fmessagey("convert", function($log) {
-    function convertMinutesToHour(time) {
-        var m, h;
-        m = time%60;
-        h = (time - m)/60;
-        return (h + "h " + m + "min");       
-    }
-    return {timeHMFormat : convertMinutesToHour};
-})
